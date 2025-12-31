8e60ec6e4380a4eb78c647c5739a2c7a46493a2c
Aashish Raj <113645205+aashishrajdev@users.noreply.github.com>
2025-12-31 12:14:20 +0530

    feat: Establish core data models with Prisma schema and implement main application layout.

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,15 +1,13 @@
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className="">
-        {/* Global background glow */}
-        <div className="fixed inset-0 -z-10 overflow-hidden">
-          <div className="absolute -top-40 -left-40 h-[500px] w-[500px] rounded-full bg-purple-600/30 blur-[120px]" />
-          <div className="absolute top-1/3 -right-40 h-[500px] w-[500px] rounded-full bg-indigo-500/30 blur-[120px]" />
-        </div>
+    <div className="relative">
+      {/* Global background glow */}
+      <div className="fixed inset-0 -z-10 overflow-hidden">
+        <div className="absolute -top-40 -left-40 h-[500px] w-[500px] rounded-full bg-purple-600/30 blur-[120px]" />
+        <div className="absolute top-1/3 -right-40 h-[500px] w-[500px] rounded-full bg-indigo-500/30 blur-[120px]" />
+      </div>
 
-        {children}
-      </body>
-    </html>
+      {children}
+    </div>
   );
 }
